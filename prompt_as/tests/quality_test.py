97399a2ad6b8cac7903862a6bbe6ced08a120e77
Onur ULUSOY <atadogan06@gmail.com>
2024-08-06 12:41:00 +0300

    feat: Added quality_test mechanism

--- prompt_as/tests/quality_test.py
+++ prompt_as/tests/quality_test.py
@@ -1,14 +1,15 @@
-import sys
-import os
-
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from tests.test_system import Test
-
-from prompt_as.prompt_as_f_decorator import prompt_as_f
 
 
-@prompt_as_f
+try:
+    from .test_system import Prompt_As_Test_System
+    from ..prompt_as_f_decorator import pf
+except ImportError:
+    from test_system import Prompt_As_Test_System
+    from prompt_as_f_decorator import pf
+
+
+
+@pf
 def save_user(username:str, password:str):
     """
     Saves the given username and pass to an sqlite database.
@@ -16,7 +17,7 @@ def save_user(username:str, password:str):
     """
 
 
-@prompt_as_f
+@pf
 def check_password(username:str, password:str):
     """
     Checks the given password is correct for given username username in the sqllite database.
@@ -29,7 +30,7 @@ def check_password(username:str, password:str):
 
 
 
-@prompt_as_f
+@pf
 def my_ip():
     """
     Returns the IP of machine, if there is an exception it should return None
@@ -40,7 +41,7 @@ def my_ip():
 
 
 
-@prompt_as_f
+@pf
 def hackernews():
     """
     Returns the latest hacker news posts (10) in a list format with their links. If there is an exception return None
@@ -48,7 +49,7 @@ def hackernews():
 
 
 
-@prompt_as_f
+@pf
 def encrypt_via_fernet(data:str, encryption_key:str) -> str:
     """
     Encrypts data with encryption_key string and returns the encrypted data.
@@ -57,7 +58,7 @@ def encrypt_via_fernet(data:str, encryption_key:str) -> str:
     If succed you will return string to me.
     """
 
-@prompt_as_f
+@pf
 def decrypt_via_fernet(encrypted_data:str, encryption_key:str) -> str:
     """
     Decrypts data with encryption_key string and returns the decrypted data. 
@@ -72,31 +73,27 @@ def decrypt_via_fernet(encrypted_data:str, encryption_key:str) -> str:
 
 
 
+def quality_test():
+
+    test_1 = Prompt_As_Test_System(hackernews, [([],{})], None, [None]).run_test()
+
+    test_2 = Prompt_As_Test_System(my_ip, [([],{})], None, [None]).run_test()
+
+    test_3 = Prompt_As_Test_System(save_user, [(["onuratakan", "6431"],{})], [True], [None]).run_test()
 
 
+    test_4 = Prompt_As_Test_System(check_password, [(["onuratakan", "13532"],{})], [False], [None]).run_test()
 
 
+    test_5 = Prompt_As_Test_System(check_password, [(["onuratakan", "6431"],{})], [True], [None]).run_test()
 
 
-test_1 = Test(hackernews, [([],{})], None, [None]).run_test()
+    print("\n\n\n\n\n*******************")
 
-test_2 = Test(my_ip, [([],{})], None, [None]).run_test()
+    print("All results:\n")
 
-test_3 = Test(save_user, [(["onuratakan", "6431"],{})], [True], [None]).run_test()
-
-
-test_4 = Test(check_password, [(["onuratakan", "13532"],{})], [False], [None]).run_test()
-
-
-test_5 = Test(check_password, [(["onuratakan", "6431"],{})], [True], [None]).run_test()
-
-
-print("\n\n\n\n\n*******************")
-
-print("All results:\n")
-
-print(test_1)
-print(test_2)
-print(test_3)
-print(test_4)
-print(test_5)
+    print(test_1)
+    print(test_2)
+    print(test_3)
+    print(test_4)
+    print(test_5)
