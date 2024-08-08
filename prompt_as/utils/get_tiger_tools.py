1787ea6c05fb5f2103c0373918b462601a429b43
gokborayilmaz <bmeyen125@gmail.com>
2024-08-08 15:29:13 +0300

    fix: fixed get_tiger_tools

--- prompt_as/utils/get_tiger_tools.py
+++ prompt_as/utils/get_tiger_tools.py
@@ -4,7 +4,7 @@ from upsonic import Tiger
 the_tiger_tools = None
 
 def get_tiger_tools():
-    return []
+
     global the_tiger_tools
     if the_tiger_tools == None:
         the_tiger_tools = Tiger().crewai(prefix="interpreter")
